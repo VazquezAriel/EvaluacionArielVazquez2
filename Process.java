a4c25078af6cb66c025ed7af45c293ebeb3d26c5
VazquezAriel <ariel.renato.av@gmail.com>
2023-11-21 19:39:36 -0500

    Resolucion del ejercicio

--- Process.java
+++ Process.java
@@ -10,18 +10,20 @@ public class Process {
 
     /**  El algoritmo compara si el valor absoluto de la diferencia entre la fila tanto de la posicion inicial como la final es igual 
         al valor absoluto de la diferencia entre la columna tanto inicial como la final de las posiciones ingresadas.**/
-    public boolean ValidatePosition() {
+    public void ValidatePosition() {
 
         int columnInicial = posicionInicial.charAt(0) - 'a';
         int rowInicial = Character.getNumericValue(posicionInicial.charAt(1)) - 1;
 
         int columnFinal = posicionFinal.charAt(0) - 'a';
         int rowFinal = Character.getNumericValue(posicionFinal.charAt(1)) - 1;
-
-        System.out.println(columnInicial + " - " + rowInicial);
-        System.out.println(columnFinal + " - " + rowFinal);
         
-        return true;
+        if (Math.abs(columnFinal - columnInicial) == Math.abs(rowFinal - rowInicial)) {
+            System.out.println("- El movimiento es correcto");
+            return;
+        } 
+
+        System.out.println("- El movimiento es incorrecto");
 
     }
     
